218b2d4407a183a318e9252aab2d2a12bde2e9cf
Siton, Lior (ls217w) <ls217w@intl.att.com>
2018-08-14 23:30:43 +0300

    [table reservation ] -------------------------------------- add Dao and menu

---
+++ table_order/TableReservation/src/main/resources/init.sql
@@ -0,0 +1,72 @@
+
+
+create database tableResrvation;
+
+use tableResrvation;
+
+
+
+create table resturant (
+id int not null auto_increment,
+name varchar(20) not null,
+PRIMARY KEY (id)
+);
+
+
+
+
+create table tod (
+id int not null auto_increment,
+order_time int not null,
+order_date date not null,
+PRIMARY KEY (id)
+);
+
+
+
+
+
+create table table_in_resturant(
+id int not null,
+size int not null,
+order_time int ,
+resturant int not null,
+PRIMARY KEY (id),
+FOREIGN KEY (order_time) REFERENCES tod(id),
+FOREIGN KEY (resturant) REFERENCES resturant(id)
+);
+
+
+
+
+create table orders(
+id int not null auto_increment,
+order_time int not null,
+people_number int not null,
+order_name varchar(20) not null,
+telephone_number varchar(10) not null,
+table_to_order int ,
+PRIMARY KEY (id),
+FOREIGN KEY (order_time) REFERENCES tod(id),
+FOREIGN KEY (table_to_order) REFERENCES table_in_resturant(id)
+);
+
+
+
+insert into resturant (name) values ('mamaia');
+
+insert into table_in_resturant values(1,4,null,1);
+
+insert into table_in_resturant values(2,4,null,1);
+
+insert into table_in_resturant values(3,4,null,1);
+
+insert into table_in_resturant values(4,6,null,1);
+insert into table_in_resturant values(5,4,null,1);
+
+
+
+insert into table_in_resturant values(6,4,null,1);
+
+insert into table_in_resturant values(7,8,null,1);
+
