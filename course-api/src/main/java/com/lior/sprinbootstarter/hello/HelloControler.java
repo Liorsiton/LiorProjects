cbf90b903afda0cc3c15af72c454468b1999071c
Siton, Lior (ls217w) <ls217w@intl.att.com>
2018-08-21 22:28:08 +0300

    [spring boot] -------------------------------------- unit1 completed

---
+++ course-api/src/main/java/com/lior/sprinbootstarter/hello/HelloControler.java
@@ -0,0 +1,15 @@
+package com.lior.sprinbootstarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloControler {
+	
+	
+	@RequestMapping("/hello")
+	public String sayHi(){
+		return "Hi";
+	}
+
+}
