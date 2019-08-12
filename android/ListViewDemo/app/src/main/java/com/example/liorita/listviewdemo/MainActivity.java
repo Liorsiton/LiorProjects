6eec1eb895b6b94699a7f6028ef7e137b0ff5b08
liorsiton <liors35@gmail.com>
2019-08-12 23:59:11 +0300

    add more  android projects

---
+++ android/ListViewDemo/app/src/main/java/com/example/liorita/listviewdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.liorita.listviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.myListView);
+
+        List<String> myList = new ArrayList<>();
+    }
+}
---
+++ java-script/targil.js
@@ -0,0 +1,6 @@
+
+let myVar = 'me not know java script';
+myVar = 'I know java script';
+
+
+console.log(myVar);
